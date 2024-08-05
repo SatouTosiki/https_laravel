6fa1dbf82e46570e9b4a494e8f41831f57b1b319
tosiki <tosiki5880@gamil.com>
2024-08-06 02:34:28 +0900

    headerの共通部分完成

--- src/laravelozi/resources/views/layouts/app.blade.php
+++ src/laravelozi/resources/views/layouts/app.blade.php
@@ -52,28 +52,28 @@
                                 </li>
                             @endif
                         @else
-                        <li class="nav-item dropdown">
-                            ログイン中のユーザ名
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <a class="dropdown-item" href="{{ route('createpage') }}">
-                                    {{ __('作成ページ') }}
+                            <li class="nav-item dropdown">
+                                ログイン中のユーザ名
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route ('createpage')}}">
+                                      
+                                        {{ __('作成ページ') }}
+                                    </a>
 
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                 </div>
