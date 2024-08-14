c2a572e0cc8eecb9777644d5e03128960893d2dc
tosiki <tosiki5880@gamil.com>
2024-08-15 00:01:15 +0900

    ui変更

--- src/laravelozi/public/assets/js/create.js
+++ src/laravelozi/public/assets/js/create.js
@@ -1,16 +1,55 @@
-let optionCount =1;
-        function addOption() {
-            if (optionCount >= 4) {
-                alert('選択肢は4つまでです。');
-                return;
-            }
-            optionCount++;
-            const optionDiv = document.createElement('div');
-            optionDiv.classList.add('option');
-            optionDiv.innerHTML = `
-                <input type="file" id="image_${optionCount}" name="options[${optionCount - 1}][image]" required>
-                
-                <input type="text" id="text_${optionCount}" name="options[${optionCount - 1}][text]" required>
-            `;
-            document.getElementById('options').appendChild(optionDiv);
+document.addEventListener('DOMContentLoaded', function() {
+    let optionCount = 1;
+
+    // 初期のボタンにイベントを設定
+    setFileInputListener('image_1', document.querySelector('.fileButton'));
+
+    function addOption() {
+        if (optionCount >= 4) {
+            alert('選択肢は4つまでです。');
+            return;
         }
+        optionCount++;
+
+        const optionDiv = document.createElement('div');
+        optionDiv.classList.add('option');
+        optionDiv.innerHTML = `
+            <div>
+                <input type="file" id="image_${optionCount}" name="options[${optionCount - 1}][image]" multiple accept="image/*" style="display:none" />
+                <button class="fileButton" type="button">
+                    <span class="button_text">画像</span>
+                    <span class="button_icon">
+                        <ion-icon name="add-circle-outline"></ion-icon>
+                    </span>
+                </button>
+            </div>
+            <div class="textStyle">
+                <input type="text"  id="text_${optionCount}" name="options[${optionCount - 1}][text]" required>
+            </div>
+        `;
+        document.getElementById('options').appendChild(optionDiv);
+
+        // 新しく追加されたボタンにイベントを設定
+        setFileInputListener(`image_${optionCount}`, optionDiv.querySelector('.fileButton'));
+    }
+
+    function setFileInputListener(inputId, fileSelectButton) {
+        const fileInput = document.getElementById(inputId);
+
+        fileSelectButton.addEventListener('click', function() {
+            fileInput.click();
+        });
+
+        fileInput.addEventListener('change', function() {
+            if (fileInput.files && fileInput.files[0]) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    fileSelectButton.innerHTML = `<img src="${e.target.result}" alt="選択した画像" class="button_img">`;
+                }
+                reader.readAsDataURL(fileInput.files[0]);
+            }
+        });
+    }
+
+    window.addOption = addOption; // addOption関数をグローバルに公開
+});
--- src/laravelozi/resources/views/create/createpage.blade.php
+++ src/laravelozi/resources/views/create/createpage.blade.php
@@ -5,8 +5,13 @@
 
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+JP&family=Tsukimi+Rounded&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('assets/css/create.css')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/create/create.css')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/create/button/createbutton.css')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/create/button/pulsbutton.css')}}">
+<link rel="stylesheet" href="{{ asset('assets/css/create/button/pushbutton.css')}}">
 
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script src="{{ asset('/assets/js/create.js')}}"></script>
 <body>
     <h1 class="textColor">アンケート作成page</h1>
@@ -17,13 +22,31 @@
         @csrf
         <div id="options">
             <div class="option">
-                <input type="file" id="image_1" name="options[0][image]" required>
-                <input type="text" id="text_1" name="options[0][text]" required>
+                <div>
+                    <input type="file" id="image_1" name="options[0][image]" multiple accept="image/*" style="display:none" />
+                    <button  class="fileButton" type="button">
+                        <span class="button_text">画像</span>
+                        <span class="button_icon">
+                            <ion-icon name="add-circle-outline"></ion-icon>
+                        </span>
+                    </button>
+                </div>
+                <div class="textStyle">
+                    <input type="text" id="text_1" name="options[0][text]" required>
+                </div>
             </div>
         </div>
-        <button type="button" onclick="addOption()">選択肢を増やす</button>
-        <br>
-        <button type="submit">投稿</button>
+    <div class="button">
+  <button type="button" onclick="addOption()">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"></path>
+    </svg>
+  </button>
+</div>
+    <br>
+    <div id="pushbutton">
+    <button class="pushButton" type="submit">投稿</button>
+    </div>
     </form>
 
 
