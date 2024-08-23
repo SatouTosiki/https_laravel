33d0340976b54acee5f6f9d4f6033487c330bf79
tosiki <tosiki5880@gamil.com>
2024-08-23 19:01:13 +0900

    webのdockerfile消えてたから終始

--- src/laravelozi/app/Http/Controllers/logcheck.php
+++ src/laravelozi/app/Http/Controllers/logcheck.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
+
 
 class logcheck extends Controller
 {
@@ -12,7 +12,7 @@ class logcheck extends Controller
         if (Auth::check()) {
             return redirect()->route('createpage');
         } else {
-            return redirect()->route('login')->with('alert', 'ログインしてください');
+            return redirect()->route('login');
         }
     }
 }
