873c5d73caef050ed56e2190107c05010dfcbb56
tosiki <tosiki5880@gamil.com>
2024-10-25 12:00:28 +0900

    四つまで投稿できた

--- src/laravelozi/app/Http/Controllers/PostController.php
+++ src/laravelozi/app/Http/Controllers/PostController.php
@@ -15,26 +15,25 @@ class PostController extends Controller
             'text2' => 'required|string|max:10',
             'text3' => 'nullable|string|max:10',
             'text4' => 'nullable|string|max:10',
-            // 'image1_path' => 'nullable|image|max:2048',
-            // 'image2_path' => 'nullable|image|max:2048',
-            // 'image3_path' => 'nullable|image|max:2048',
-            // 'image4_path' => 'nullable|image|max:2048',
         ]);
 
         // 新しい Post モデルを作成し、データを保存
-        $post = new Post;
+        //img〇はcreatepage.blade.phpのnameから引用している
+
         $img1_path = $request->file('img1')->store('public/images');
         $img2_path = $request->file('img2')->store('public/images');
         $img3_path = $request->file('img3')->store('public/images');
         $img4_path = $request->file('img4')->store('public/images');
+
+        $post = new Post;
 
         $post->title = $validatedData['title'];
         $post->text1 = $validatedData['text1'];
         $post->text2 = $validatedData['text2'];
         $post->text3 = $validatedData['text3'] ?? null;
         $post->text4 = $validatedData['text4'] ?? null;
-        $post->image1_path = $img1_path ?? null;
-        $post->image2_path = $img2_path ?? null;
+        $post->image1_path = $img1_path;
+        $post->image2_path = $img2_path;
         $post->image3_path = $img3_path ?? null;
         $post->image4_path = $img4_path ?? null;
 
